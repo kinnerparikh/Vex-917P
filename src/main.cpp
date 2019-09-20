92e2828b473d95afc253dd83a3cb2ac95108405a
Kinner Parikh <kinnerpa@gmail.com>
2019-09-19 17:34:06 -0700

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -63,10 +63,13 @@ void autonomous( void ) {
 
 void usercontrol( void ) {
   // User control code here, inside the loop
-  while (1) {
+  while (true) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo 
     // values based on feedback from the joysticks.
+
+    Brain.Screen.print("Hello World");
+    Brain.Screen.print("This is actually working what the fuck.");
 
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to 
