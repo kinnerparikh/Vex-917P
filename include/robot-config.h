5a3720828a3665234696ecf7d1c5e7de5408fe6a
Daniel Han <s-dhan@lwsd.org>
2019-12-12 16:12:09 -0800

    asdf

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,15 +3,6 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
-extern controller Controller1;
-extern motor LeftDR4BMotor;
-extern motor LeftDriveMotor;
-extern motor LeftIntakeMotor;
-extern motor CubeTrayMotor;
-extern motor RightIntakeMotor;
-extern motor CenterDriveMotor;
-extern motor RightDriveMotor;
-extern motor RightDR4BMotor;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,15 +8,6 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-controller Controller1 = controller(primary);
-motor LeftDR4BMotor = motor(PORT1, ratio36_1, true);
-motor LeftDriveMotor = motor(PORT2, ratio18_1, true);
-motor LeftIntakeMotor = motor(PORT3, ratio18_1, false);
-motor CubeTrayMotor = motor(PORT4, ratio36_1, true);
-motor RightIntakeMotor = motor(PORT5, ratio18_1, true);
-motor CenterDriveMotor = motor(PORT6, ratio18_1, false);
-motor RightDriveMotor = motor(PORT10, ratio18_1, false);
-motor RightDR4BMotor = motor(PORT21, ratio36_1, false);
 
 // VEXcode generated functions
 
